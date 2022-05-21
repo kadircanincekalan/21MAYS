8d0d59efc75866845a5054bfb8650530719877bf
kadircanincekalan <kadircanincekalan@gmail.com>
2022-05-21 12:48:18 +0300

    second part

--- fscohort/views.py
+++ fscohort/views.py
@@ -1,6 +1,8 @@
 
-from django.shortcuts import render
+from pdb import post_mortem
+from django.shortcuts import redirect, render
 from .models import Student
+from .forms import StudentForm
 
 def index(request):
     return render(request, 'fscohort/index.html')
@@ -14,3 +16,18 @@ def student_list(request):
     }
     
     return render(request, "fscohort/student_list.html", context)
+
+def student_add(request):
+    form = StudentForm()
+    print(request.POST)
+    if  request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("list")
+        pass
+    
+    context = {
+        "form" : form
+    }
+    return render(request, "fscohort/student_add.html", context)
