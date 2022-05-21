6439ed7de7e591f04662db18182e394bc7d478ca
kadircanincekalan <kadircanincekalan@gmail.com>
2022-05-21 12:00:05 +0300

    add student model and read

---
+++ fscohort/views.py
@@ -0,0 +1,16 @@
+
+from django.shortcuts import render
+from .models import Student
+
+def index(request):
+    return render(request, 'fscohort/index.html')
+
+
+def student_list(request):
+    students = Student.objects.all()
+    
+    context = {
+        "students" : students
+    }
+    
+    return render(request, "fscohort/student_list.html", context)
